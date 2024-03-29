43c33d5f59ac46bbfa7bc1b00b8c92b4494edac1
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-03-29 17:39:18 +0600

    add extra folder and added intercepting

---
+++ app/folder1/folder2/(..)folder3/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1 className='text-2xl text-purple-700'>this is intercepting folder 1 page, if you want to see magic, pls hard refresh this page</h1>
+        </div>
+    );
+};
+
+export default page;
--- app/folder1/folder2/page.js
+++ app/folder1/folder2/page.js
@@ -5,7 +5,9 @@ const page = () => {
     return (
         <div>
             <h1>this is folder 2</h1> <br/>
-            <Link className='text-red-500' href="/folder1">Back folder 1</Link>
+            <Link className='text-red-500' href="/folder1">Back folder 1</Link> <br/> <br/>
+
+            <Link className='text-blue-500' href="/folder1/folder3">navigate folder 3</Link>
         </div>
     );
 };
